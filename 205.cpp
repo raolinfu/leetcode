ed98b311e01e742375423c6a174b154edfea54ec
raolinhu <1145333729@qq.com>
2021-10-09 14:35:11 +0800

    first ci

---
+++ 205.cpp
@@ -0,0 +1,8 @@
+#include"add.h"
+int add(int a, int b){
+    return a + b + m;
+}
+
+int sub(int a, int b){
+	return a - b;
+}
---
+++ add.h
@@ -0,0 +1,5 @@
+const int m = 100;
+int add(int a, int b);
+
+int sub(int a, int b);
+
