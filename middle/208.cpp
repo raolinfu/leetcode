9cb1b387cf479f6724ecc1d1d81a7b6cbe373ce8
raolinhu <1145333729@qq.com>
2021-10-23 19:38:37 +0800

    单词拆分

--- middle/208.cpp
+++ middle/208.cpp
@@ -1,109 +1,54 @@
 #include<string>
+#include<unordered_map>
 #include<vector>
 #include<algorithm>
 #include<map>
 
 using namespace std;
 
-struct Word{
-	char ch;
-	bool isEnd;
-	vector<Word*> next;
-	Word(char c):ch(c), next(26, nullptr), isEnd(false){
-	}
-};
-
 class Trie {
+private:
+	string word;
+	unordered_map<char, Trie*> children;
+	Trie* searchPrefix(string prefix){
+		Trie *node = this;
+		for(auto ch: prefix){
+			if(node->children.count(ch))
+				node = node->children[ch];
+			else
+				return nullptr;
+		}
+
+		return node;
+	}
 public:
     Trie() {
-
+		this->word = "";
     }
     
     void insert(string word) {
-		if(word.empty())
-			return;
-		if(mp.find(*word.begin()) == mp.end())
-		{	
-			Word *head = new Word(*word.begin());
-			mp.insert({*word.begin(), head});
-			for(auto i = 1; i < word.length(); i++){
-				Word *tmp = new Word(word[i]);
-				head->next[word[i] - 'a'] = tmp;
-				head = tmp;
-				if(i == word.length() - 1)
-					tmp->isEnd = true;
+		Trie *node = this;
+		for(auto ch: word){
+			if(node->children[ch] == nullptr){
+				node->children[ch] = new Trie();	
 			}
-		}else{
-			Word *head = mp[word[0]];
-			int i = 1;
-			while(head->next[word[i] - 'a']){
-				if(i == word.length() - 1)
-				{
-					head->isEnd = true;
-					return;
-				}
-				head = head->next[word[i] - 'a'];
-				i++;
-			}
-			while(i < word.length()){
-				Word *tmp = new Word(word[i]);
-				head->next[word[i] - 'a'] = tmp;
-				head = tmp;
-				if(i == word.length() - 1)
-					tmp->isEnd = true;
-			}
+			node = node->children[ch];
 		}
+		node->word = word;
 	}
     
     bool search(string word) {
-		if(word.empty())
-			return false;
-		if(mp.find(word[0]) == mp.end())
-			return false;
-		else{
-			Word *head = mp[word[0]];
-			int i = 1;
-			head = head->next[word[i] - 'a'];
-			while(head && head->ch == word[i]){
-				if(i == word.length() - 1 && head->isEnd)
-					return true;
-				head = head->next[word[i] - 'a'];
-				i++;
-			}
-		}
-
-		return false;
+		Trie* node = searchPrefix(word);
+		return node != nullptr && node->word == word;
     }
     
     bool startsWith(string prefix) {
-		if(prefix.empty())
-			return false;
-		if(mp.find(prefix[0]) == mp.end())
-			return false;
-		else{
-			Word *head = mp[prefix[0]];
-			for(int i = 0; i < prefix.length(); i++){
-				if(head && prefix[i] == head->ch)
-				{
-					head = head->next[prefix[i] - 'a'];
-					continue;
-				}
-				return false;
-			}
-		}
-
-		return true;
+		Trie *node = searchPrefix(prefix);
+		return node == nullptr? false: true;
     }
 
-private:
-	map<char, Word*> mp;
 };
 
 int main(){
-	vector<int> nums(61);
-	Trie trie;
-	trie.insert("rao");
-	bool res = trie.search("rao");
-
 	return 0;
 }
