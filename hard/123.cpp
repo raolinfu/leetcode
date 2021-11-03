121a23a50ddf373684444aaa8a5613bde53a0d33
raolinhu <1145333729@qq.com>
2021-11-03 22:34:12 +0800

    买卖股票最佳时机

--- hard/123.cpp
+++ hard/123.cpp
@@ -4,44 +4,36 @@
 
 using namespace std;
 
-void process(vector<int> &nums){
+int maxProfit(vector<int> &prices){
 	int res = INT_MIN;
-	if(nums.size() == 0)
-		return;
-	vector<int> dp(nums.size(), 0);
-	vector<int> dp1(nums.size(), 0);
-	int minNum = nums[0];
-	int maxNum = nums[nums.size() - 1];
-	for(int i = 1; i < nums.size(); i++){
-		minNum = min(minNum, nums[i]);
-		dp[i] = nums[i] - minNum > dp[i - 1]? nums[i] - minNum: dp[i - 1];
+	if(prices.size() == 0)
+		return 0;
+	vector<int> dp(prices.size(), 0);
+	vector<int> dp1(prices.size(), 0);
+	int minNum = prices[0];
+	int maxNum = prices[prices.size() - 1];
+	for(int i = 1; i < prices.size(); i++){
+		minNum = min(minNum, prices[i]);
+		dp[i] = prices[i] - minNum > dp[i - 1]? prices[i] - minNum: dp[i - 1];
 	}
-	for(int i = nums.size() - 2; i >= 0; i--){
-		maxNum = max(maxNum, nums[i]);
-		dp1[i] = maxNum - nums[i] > dp1[i + 1]? maxNum - nums[i]: dp1[i + 1];
+	for(int i = prices.size() - 2; i >= 0; i--){
+		maxNum = max(maxNum, prices[i]);
+		dp1[i] = maxNum - prices[i] > dp1[i + 1]? maxNum - prices[i]: dp1[i + 1];
 	}
-	for(int i = 0; i < nums.size(); i++){
-		cout<< dp[i]<< "\t";
-	}
-	cout << endl;
-	for(int i = 0; i < nums.size(); i++){
-		cout<< dp1[i]<< "\t";
-	}
-	cout << endl;
-	for(int i = 0; i < nums.size(); i++){
+	for(int i = 0; i < prices.size(); i++){
 		res = max(res, dp[i] + dp1[i]);
 	}
-	cout<< res<< endl;
 
+	return res;
 }
 
 int main(){
 	vector<int> nums = {1, 6, 3, 43, 45, 23};
 	// vector<int> nums = { 6, 5, 3};
-	process(nums);
+	cout<< maxProfit(nums)<< endl;
 	vector<int> prices = {1,2,3,4,5};
-	process(prices);
+	cout<< maxProfit(prices)<< endl;
 	vector<int> prices1 = {3,3,5,0,0,3,1,4};
-	process(prices1);
+	cout<< maxProfit(prices1)<< endl;
 	return 0;
 }
