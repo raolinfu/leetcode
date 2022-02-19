b0b57a49da673d026e5210700fb99d62d5afc3a1
raolinhu <1145333729@qq.com>
2022-02-19 21:47:12 +0800

    双指针

---
+++ middle/825.cpp
@@ -0,0 +1,26 @@
+#include<vector>
+#include<iostream>
+#include<algorithm>
+using namespace std;
+class Solution {
+public:
+    int numFriendRequests(vector<int>& ages) {
+        int n = ages.size();
+        sort(ages.begin(), ages.end());
+        int left = 0, right = 0, ans = 0;
+        for (int age: ages) {
+            if (age < 15) {
+                continue;
+            }
+            while (ages[left] <= 0.5 * age + 7) {
+                ++left;
+            }
+            while (right + 1 < n && ages[right + 1] <= age) {
+                ++right;
+            }
+            ans += right - left;
+        }
+        return ans;
+    }
+};
+
