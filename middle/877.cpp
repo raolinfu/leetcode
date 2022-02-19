8e9d56e161f52fac2f54a03dd057d0d006973b69
raolinhu <1145333729@qq.com>
2022-02-19 11:54:28 +0800

    预测赢家

---
+++ middle/877.cpp
@@ -0,0 +1,25 @@
+#include<vector>
+#include<iostream>
+using namespace std;
+// dp[i][j] = max(piles[i] - dp[i + 1][j], piles[j] - dp[i][j - 1])
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+		int len = piles.size();
+		vector<vector<int>> dp(len, vector<int>(len, 0));
+		for(int i = 0; i < len; i++){
+			dp[i][i] = piles[i];
+		}
+		for(int i = len - 1; i >= 0; i--){
+			for(int j = i + 1; j < len; j++){
+				dp[i][j] = max(piles[i] - dp[i + 1][j], piles[j] - dp[i][j - 1]);
+			}
+		}
+
+		return dp[0][len - 1] > 0;
+    }
+};
+
+int main(){
+	return 0;
+}
