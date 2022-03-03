33a315199017dc65f2ab313636d72eae12493eac
linhu-intel <linhu.rao@intel.com>
2022-03-03 17:02:27 +0800

    bfs和备忘数组

---
+++ middle/001.cpp
@@ -0,0 +1,61 @@
+#include<vector>
+#include<unordered_map>
+#include<string>
+#include<algorithm>
+#include<iostream>
+using namespace std;
+class Solution{
+	public:
+		bool splitArr(vector<int>& nums, int k){
+			int sum  = 0;
+			for(auto n: nums)
+				sum += n;
+			if(sum % k != 0)
+				return false;
+			string s(nums.size(), '0');
+			sort(nums.begin(), nums.end());
+			int base = sum / k;
+			return helper(nums, base, s, k, base ,0);
+		}
+		bool helper(vector<int>& nums, int target, string used, int &k, int& base, int cnt){
+			if(cnt == k)
+				return true;
+			if(mp.count(used))
+				return false;
+
+			for(int i = 0; i < nums.size(); i++){
+				if(used[i] == '1'){
+					continue;
+				}
+				else if(used[i] == '0' && nums[i] > target){
+					break;
+				}
+				else if(used[i] == '0' && nums[i] == target){
+					string tmp = used;
+					tmp[i] = '1';
+					if(helper(nums, base, tmp, k, base, cnt + 1))
+						return true;
+				}
+				else if(used[i] == '0' && nums[i] < target){
+					string tmp = used;
+					tmp[i] = '1';
+					if(helper(nums, target - nums[i], tmp, k, base, cnt))
+						return true;
+				}
+			}
+
+			mp[used] = false;
+			return false;
+		}
+	private:
+		unordered_map<string, bool> mp;
+};
+
+int main(){
+	Solution s;
+	vector<int> nums{1, 3, 4, 6, 2, 5, 3};
+	int k = 4;
+	cout<< s.splitArr(nums, k)<< endl;
+
+	return 0;
+}
---
+++ middle/752.cpp
@@ -0,0 +1,63 @@
+#include<string>
+#include<iostream>
+#include<unordered_set>
+#include<queue>
+#include<vector>
+using namespace std;
+class Solution{
+	public:
+		int openLock(vector<string>& deadends, string target){
+			int ans = 0;
+			queue<string> q;
+			unordered_set<string> set(deadends.begin(), deadends.end());
+			q.push("0000");
+			while(!q.empty()){
+				int len = q.size();
+				for(int k = 0; k < len; k++){
+					string cur = q.front();
+					q.pop();
+					if(cur == target)
+						return ans;
+					for(int i = 0; i < 4; i++){
+						auto tmp = spin(cur, i);
+						if(!set.count(tmp.first)){
+							set.insert(tmp.first);
+							q.push(tmp.first);
+						}
+						if(!set.count(tmp.second)){
+							set.insert(tmp.second);
+							q.push(tmp.second);
+						}
+					}
+				}
+				ans++;
+			}
+
+			return -1;
+		}
+
+		pair<string, string> spin(string& str, int i){
+			string str1 = str;
+			string str2 = str;
+			if(str[i] == '0'){
+				str1[i] = '1';
+				str2[i] = '9';
+				return {str1, str2};
+			}else if(str[i] == '9'){
+				str1[i] = '0';
+				str2[i] = '8';
+				return {str1, str2};
+			}else {
+				str1[i] -= 1;
+				str2[i] += 1;
+				return {str1, str2};
+			}
+		}
+};
+int main(){
+	Solution s;
+	vector<string> deadends = {"0101", "0102", "0201", "1212", "2002"};
+	string target = "0202";
+	cout<< s.openLock(deadends, target)<< endl;
+	return 0;
+}
