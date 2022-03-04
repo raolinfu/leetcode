1df2a063b47097c0e7b711c1e31064ff35f1466a
linhu-intel <linhu.rao@intel.com>
2022-03-04 16:29:33 +0800

    递归链表

---
+++ middle/92.cpp
@@ -0,0 +1,35 @@
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* reverseBetween(ListNode* head, int left, int right) {
+		if(left == 1)
+			return reverseN(head, right);
+		head->next = reverseBetween(head->next, left - 1, right - 1);
+		return head;
+    }
+
+	ListNode* reverseN(ListNode* head, int n){
+		if(n == 1){
+			succsor = head->next;
+			return head;
+		}
+
+		ListNode* last = reverseN(head->next, n - 1);
+		head->next->next = head;
+		head->next = succsor;
+		return last;
+	}
+
+private:
+		ListNode* succsor = nullptr;
+
+
+};
