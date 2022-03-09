5dde3da1389d7ceffbee8bc254df951919476793
linhu-intel <linhu.rao@intel.com>
2022-03-09 09:49:47 +0800

    矩阵中的最大正方形

--- middle/221.cpp
+++ middle/221.cpp
@@ -2,66 +2,65 @@
 #include<vector>
 using namespace std;
 
-vector<vector<int>> getLeft(vector<vector<char>>& matrix, int &rows, int &cols);
-vector<vector<int>> getDown(vector<vector<char>>& matrix, int &rows, int &cols);
-int maximalSquare(vector<vector<char>>& matrix) {
-	int res = 0;
-	int rows = matrix.size();
-	int cols = matrix[0].size();
-	vector<vector<int>> left = getLeft(matrix, rows, cols);
-	vector<vector<int>> down = getDown(matrix, rows, cols);
-
-	return res;
-}
-
-vector<vector<int>> getLeft(vector<vector<char>>& matrix, int &rows, int &cols){
-	vector<vector<int>> left;
-	for(int i = 0; i < rows; i++)
-		left.push_back(vector<int>(cols, 0));
-	for(int i = rows - 1; i >=0; i--){
-		int cnt = 0;
-		for(int j = cols - 1; j >=0; j--){
-			if(matrix[i][j] == '1'){
-				cnt += 1;
+class Solution {
+public:
+    int maximalSquare(vector<vector<char>>& matrix) {
+		int ans;
+		int rows = matrix.size();
+		int cols = matrix[0].size();
+		vector<vector<int>> grid(rows, vector<int>(cols));
+		for(int i = 0; i < rows; i++){
+			for(int j = 0; j < cols; j++){
+				if(i == 0)
+					grid[i][j] = matrix[i][j] == '1'? 1: 0;
+				else if(matrix[i][j] == '0'){
+					grid[i][j] = 0;
+				}
+				else if(matrix[i][j] == '1'){
+					grid[i][j] = grid[i - 1][j] + 1;
+				}
 			}
-			else{
-				cnt = 0;
-			}
-			left[i][j] = cnt;
 		}
-	}
-	return left;
-}
-vector<vector<int>> getDown(vector<vector<char>>& matrix, int &rows, int &cols){
-	vector<vector<int>> down;
-	for(int i = 0; i < rows; i++)
-		down.push_back(vector<int>(cols, 0));
-	for(int j = cols - 1; j >=0; j--){
-		int cnt = 0;
-		for(int i = rows - 1; i >=0; i--){
-			if(matrix[i][j] == '1'){
-				cnt += 1;
-			}
-			else{
-				cnt = 0;
-			}
-			down[i][j] = cnt;
+
+		for(int i = 0; i < rows; i++){
+			int tmp = getMaxSquire(grid[i]);
+			ans = max(ans, tmp);
 		}
+
+		return ans;
+    }
+
+	int getMaxSquire(vector<int>& nums){
+		int ans = 0;
+		int i = 0;
+		vector<int> stk{0};
+		while(stk.size() > 1 && i < nums.size()){
+			while(stk.size() > 1 && nums[i] < nums[stk.back()]){
+				int idx = stk.back();
+				int len = i - stk.back();
+				if(len >= nums[idx])
+					ans = max(ans, nums[idx] * nums[idx]);
+				stk.pop_back();
+			}
+
+			stk.push_back(i);
+			i++;
+		}
+		while(stk.size() > 1){
+			int idx = stk.back();
+			int len = i - stk.back();
+			if(len >= nums[idx])
+				ans = max(ans, nums[idx] * nums[idx]);
+			stk.pop_back();
+		}
+
+		return ans;
 	}
-
-	return down;
-}
-
-vector<vector<int>> getX(vector<vector<int>>& down, int &rows, int &cols){
-	vector<vector<int>> x;
-	
-	return x;
-}
-void test(int a, int b){
-	int c = a - b;
-}
+};
 int main(){
+	Solution sol;
 	vector<vector<char>> matrix = {{'1','0','1','0','0'},{'1','0','1','1','1'},{'1','1','1','1','1'},{'1','0','0','1','0'}};
-	int res = maximalSquare(matrix);
+	int ans = sol.maximalSquare(matrix);
+	cout<< ans<< endl;
 	return 0;
 }
