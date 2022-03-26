3e77410f3b60ee68e33f6f41f374a7361d3becd0
raolinhu <1145333729@qq.com>
2022-03-26 22:23:32 +0800

    乘积最大的子数组

--- middle/152.cpp
+++ middle/152.cpp
@@ -2,36 +2,25 @@
 #include<iostream>
 #include<climits>
 using namespace std;
-
-int maxProduct(vector<int>& nums) {
-	int res = INT_MIN;
-	int mx = INT_MIN;
-	int mi = INT_MAX;
-	for(int i = 0; i < nums.size(); i++){
-		if(nums[i] > 0){
-			mx = mx == INT_MIN? nums[i]: mx * nums[i];
-			mi = mi == INT_MAX? mi: mi * nums[i];
-		}else if(nums[i] < 0){
-			int tmp = mx;
-			mx = mi == INT_MAX? INT_MIN: mi * nums[i];
-			mi = tmp == INT_MIN? nums[i]: tmp * nums[i];
-		}else{
-			res = max(res, 0);
-			mx = INT_MIN;
-			mi = INT_MAX;
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+		int great = nums[0];
+		int less = nums[0];
+		int ans = nums[0];
+		for (int i = 1; i < nums.size(); i++) {
+			if (great == 0) {
+				great = nums[i];
+				less = nums[i];
+				ans = max(ans, great);
+			} else {
+                int a = max(great * nums[i], less * nums[i]);
+                int b = min(great * nums[i], less * nums[i]);
+				great = max(a, nums[i]);
+				less = min(b, nums[i]);
+			}
 		}
 
-		res = mi == INT_MAX? res: max(res, mi);
-		res = mx == INT_MIN? res: max(res, mx);
-	}
-
-	return res;
-}
-
-int main(){
-	vector<int> nums = {12, -1, 7, 0, 2, 5};
-	int res = maxProduct(nums);
-	cout << res << endl;
-	return 0;
-}
-
+		return ans;
+    }
+};
