236091c37c502a976ce263fafbe12e7a70269d1f
raolinhu <1145333729@qq.com>
2022-04-16 18:33:48 +0800

    分数到小数

--- middle/166.cpp
+++ middle/166.cpp
@@ -1,48 +1,51 @@
-#include<iostream>
-#include<string>
-#include<map>
+#include <string>
+#include <iostream>
+#include <unordered_map>
 
 using namespace std;
+class Solution {
+public:
+    string fractionToDecimal(int numerator, int denominator) {
+        bool neg = false;
+        if ((numerator < 0 && denominator > 0) || (numerator > 0 && denominator < 0))
+            neg = true;
+        long n = abs((long)numerator / (long)denominator);
+        long n1 = abs((long)numerator % (long)denominator);
+        if (n1 == 0) {
+            return neg? '-' + std::to_string(n): std::to_string(n);
+        }
 
-string getRes(int numerator, int denominator){
-	int i = 0;
-	string res;
+        long divide = abs((long)denominator);
+        string ans = to_string(n) + ".";
+        string tmp;
+        unordered_map<long, int> mp;
+        int i = 0;
+        while (true) {
+            if (mp.count(n1)){
+                int idx = mp[n1];
+                ans += tmp.substr(0, idx);
+                ans += '(';
+                ans += tmp.substr(idx);
+                ans += ')';
+                break;
+            }
+            mp.insert({n1, i});
+            n1 *= 10;
+            if (n1 < divide) {
+                if (!mp.count(n1)) {
+                    tmp += '0';
+                }
+            } else {
+                tmp += '0' + n1 / divide;
+                n1 = n1 % divide;
+                if (n1 == 0) {
+                    ans += tmp;
+                    break;
+                }
+            }
+            i++;
+        }
 
-	while(numerator < denominator){
-		numerator *= 10;
-		res = res == ""? "0.": res + "0";
-		i++;
-	}
-
-	int pre = numerator / denominator;
-	int n = numerator % denominator;
-	map<int, int> m;
-	res += to_string(pre);
-	m.insert({pre, i});
-
-	while(n){
-		n *= 10;
-		pre = n / denominator;
-		n = n % denominator;
-		if(m.find(pre) == m.end())
-		{
-			m.insert({pre, i});
-			res += to_string(pre);
-		}
-		else{
-			res.insert(m[pre], "(");
-			res.push_back(')');
-			return res;
-		}
-		i++;
-	}
-
-	return res;
-}
-
-
-int main(){
-	string res;
-	res = getRes(1, 13);
-	return 0;
-}
+        return neg? '-' + ans: ans;
+    }
+};
