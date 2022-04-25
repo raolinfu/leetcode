1a5fc68a5f2d3fde9384f06580244cd92cc73506
raolinhu <1145333729@qq.com>
2022-04-25 21:36:52 +0800

    在圆内随机生成点

---
+++ middle/478.cpp
@@ -0,0 +1,28 @@
+#include <cmath>
+#include <random>
+using namespace std;
+class Solution {
+public:
+    double rad, xc, yc;
+    //c++11 random floating point number generation
+    mt19937 rng{random_device{}()};
+    uniform_real_distribution<double> uni{0, 1};
+
+    Solution(double radius, double x_center, double y_center) {
+        rad = radius, xc = x_center, yc = y_center;
+    }
+
+    vector<double> randPoint() {
+        double x0 = xc - rad;
+        double y0 = yc - rad;
+
+        while(true) {
+            double xg = x0 + uni(rng) * 2 * rad;
+            double yg = y0 + uni(rng) * 2 * rad;
+            if (sqrt(pow((xg - xc), 2) + pow((yg - yc), 2)) <= rad)
+                return {xg, yg};
+        }
+    }
+};
+
+
