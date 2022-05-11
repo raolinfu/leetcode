ed24f30635108e60bb835c40d4a1f8624b6f2df4
raolinhu <1145333729@qq.com>
2022-05-11 16:17:13 +0800

    在线选举

---
+++ middle/911.cpp
@@ -0,0 +1,32 @@
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+using namespace std;
+
+class TopVotedCandidate {
+public:
+    vector<int> tops;
+    vector<int> times;
+
+    TopVotedCandidate(vector<int>& persons, vector<int>& times) {
+		// 更新i时的最大选举人
+        unordered_map<int, int> voteCounts;
+        voteCounts[-1] = -1;
+        int top = -1;
+        for (auto & p : persons) {
+            voteCounts[p]++;
+            if (voteCounts[p] >= voteCounts[top]) {
+                top = p;
+            }
+            tops.emplace_back(top);
+        }
+        this->times = times;
+    }
+    
+    int q(int t) {
+        int pos = upper_bound(times.begin(), times.end(), t) - times.begin() - 1;
+        return tops[pos];
+    }
+};
+
+
