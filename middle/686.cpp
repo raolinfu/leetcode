60a2a81d1f8282bc15d9fe382d68994ccaef5952
raolinhu <1145333729@qq.com>
2022-05-11 10:27:37 +0800

    重复叠加字符串匹配,循环处理hash值

--- middle/686.cpp
+++ middle/686.cpp
@@ -3,23 +3,53 @@ using namespace std;
 
 class Solution {
 public:
+    int strStr(string haystack, string needle) {
+        int n = haystack.size(), m = needle.size();
+        if (m == 0) {
+            return 0;
+        }
+
+        long long k1 = 1e9 + 7;
+        long long k2 = 1337;
+        srand((unsigned)time(NULL));
+        long long kMod1 = rand() % k1 + k1;
+        long long kMod2 = rand() % k2 + k2;
+
+        long long hash_needle = 0;
+		// 算needle对应的 hash值
+        for (auto c : needle) {
+            hash_needle = (hash_needle * kMod2 + c) % kMod1;
+        }
+
+		// 算haystack 长度为m字串的hash值
+        long long hash_haystack = 0, extra = 1;
+		// m 可能是大于n 的  [i % n]就是循环处理
+        for (int i = 0; i < m - 1; i++) {
+            hash_haystack = (hash_haystack * kMod2 + haystack[i % n]) % kMod1;
+            extra = (extra * kMod2) % kMod1;
+        }
+
+        for (int i = m - 1; (i - m + 1) < n; i++) {
+            hash_haystack = (hash_haystack * kMod2 + haystack[i % n]) % kMod1;
+            if (hash_haystack == hash_needle) {
+                return i - m + 1;
+            }
+            hash_haystack = (hash_haystack - extra * haystack[(i - m + 1) % n]) % kMod1;
+            hash_haystack = (hash_haystack + kMod1) % kMod1;
+        }
+        return -1;
+    }
+
     int repeatedStringMatch(string a, string b) {
-		int len1 = a.length();
-		int len2 = b.length();
-		int ans = 0;
-		string tmp;
-		while (tmp.length() < len2) {
-			tmp += a;
-		}
-		while (true) {
-			while (tmp.find(b) != string::npos) {
-				return ans;
-			}
-			if (tmp.length() > len2 * 2 && tmp.length() > len1 * 2) {
-				return -1;
-			}
-			ans++;
-			tmp += a;
-		}
+        int an = a.size(), bn = b.size();
+        int index = strStr(a, b);
+        if (index == -1) {
+            return -1;
+        }
+        if (an - index >= bn) {
+            return 1;
+        }
+        return (bn + index - an - 1) / an + 2;
     }
 };
+
