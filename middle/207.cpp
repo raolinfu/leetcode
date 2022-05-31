df79bf9e63f18b32344e9e82c27525d192afc4e4
raolinhu <1145333729@qq.com>
2022-06-01 07:41:59 +0800

    课程表

--- middle/207.cpp
+++ middle/207.cpp
@@ -1,6 +1,39 @@
 #include<vector>
-
 using namespace std;
-bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
 
-}
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> graph(numCourses, vector<int>());
+        vector<bool> visited(numCourses, false);
+        vector<bool> walked(numCourses, false);
+
+        for (auto& v: prerequisites) {
+            graph[v[1]].push_back(v[0]);
+        }
+
+        for (int i = 0; i < numCourses; i++) {
+            if (!backtrace(graph, visited, walked, i))
+                return false;
+        }
+
+        return true;
+    }
+
+    bool backtrace(vector<vector<int>>& graph, vector<bool>& visited, vector<bool>& walked, int i) {
+        if (visited[i])
+            return false;
+        if (walked[i])
+            return true;
+        visited[i] = true;
+        for (auto j: graph[i]) {
+            if (!backtrace(graph, visited, walked, j)) {
+                return false;
+            }
+        }
+
+        visited[i] = false;
+        walked[i] = true;
+        return true;
+    }
+};
