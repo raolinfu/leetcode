21d14d532c7299148f2fb399e5e77d164ebf2760
raolinhu <1145333729@qq.com>
2022-06-20 20:35:05 +0800

    公交路线

--- hard/815.cpp
+++ hard/815.cpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <unordered_set>
 #include <climits>
 #include <queue>
 #include <unordered_map>
@@ -47,5 +48,52 @@ public:
             }
         }
         return ret == INT_MAX ? -1 : ret;
+    }
+
+    int numBusesToDestination1(vector<vector<int>>& routes, int source, int target) {
+        int ans = 0;
+        int n = routes.size();
+        unordered_map<int, unordered_set<int>> mp;
+        vector<bool> visited(n, false);
+        queue<int> que;
+
+        if (source == target)
+            return ans;
+
+        for (int i = 0; i < n; i++) {
+            for (auto& j: routes[i]) {
+                mp[j].insert(i);
+            }
+        }
+
+        for (auto& station: mp[source]) {
+            visited[station] = true;
+            for (auto& i: routes[station])
+                que.push(i);
+        }
+
+        ans++;
+
+        while (!que.empty()) {
+            int len = que.size();
+            for (int i = 0; i < len; i++) {
+                int f = que.front();
+                que.pop();
+
+                if (f == target)
+                    return ans;
+
+                for (auto& station: mp[f]) {
+                    if (!visited[station]) {
+                        for (auto& j: routes[station])
+                            que.push(j);
+                        visited[station] = true;
+                    }
+                }
+            }
+            ans++;
+        }
+
+        return -1;
     }
 };
