a884895c9ff6c7690e8d0c0d1909bdac8f2e84eb
raolinhu <1145333729@qq.com>
2022-07-24 16:37:37 +0800

    最高的广告牌

---
+++ hard/956.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+#include <unordered_map>
+using namespace std;
+class Solution {
+public:
+    int tallestBillboard(vector<int>& rods) {
+        unordered_map<int, int> map;
+        map[0] = 0;
+        for(auto rod :rods) {
+            unordered_map<int, int> tmp(map);
+            for(auto it : tmp) {
+                int key = it.first;
+                map[key + rod] = max(map[key + rod], tmp[key] + rod);
+                map[key - rod] = max(map[key - rod], tmp[key]);
+            }
+        }
+        return map[0];
+    }
+};
